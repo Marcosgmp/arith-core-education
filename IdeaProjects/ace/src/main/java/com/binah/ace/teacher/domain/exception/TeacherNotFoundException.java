585c826be8336aaec5b3822ca5312603094ba7be
davijsf <davif2717@gmail.com>
2026-03-12 13:20:41 -0300

    feat(teacher): add TeacherNotFoundException
    - Custom exception extendendo EntityNotFoundException (shared)
    - 2 construtores: por UUID e por CPF
    - Mensagens específicas para Teacher context

--- IdeaProjects/ace/src/main/java/com/binah/ace/teacher/domain/exception/TeacherNotFoundException.java
+++ IdeaProjects/ace/src/main/java/com/binah/ace/teacher/domain/exception/TeacherNotFoundException.java
@@ -1,4 +1,18 @@
 package com.binah.ace.teacher.domain.exception;
 
-public class TeacherNotFoundException {
+import com.binah.ace.shared.exception.EntityNotFoundException;
+import com.binah.ace.shared.valueobject.CPF;
+import com.binah.ace.teacher.domain.entity.Teacher;
+
+import java.util.UUID;
+
+public class TeacherNotFoundException extends EntityNotFoundException{
+
+    public TeacherNotFoundException(UUID id) {
+        super("Teacher not found with Id: " + id);
+    }
+
+    public TeacherNotFoundException(CPF cpf) {
+        super("Teacher not found with CPF: " + cpf);
+    }
 }
